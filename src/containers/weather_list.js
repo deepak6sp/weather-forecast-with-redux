7a30194f4c2b863185ecd3a26961aac0870f6df4
deepak6sp <deepak6sp@gmail.com>
2016-05-03 21:02:39 +1000

    forecast displayed

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -4,22 +4,31 @@ import { bindActionCreators } from 'redux';
 
 class WeatherList extends Component {
   render() {
+  	
+   var new_table = this.props.weather.map(function (element,index){
+   		return (
+  		 	<tr>
+  		 	  <td>{index}</td>
+		      <td>{element.city.name}</td>
+		      <td></td>
+		      <td></td>
+		    </tr>
+		);
+  	});
+   console.log(this.props.weather);
     return (
       <div>
       	<table className="table table-striped">
 		  <thead>
 		    <tr>
+		      <th>#</th>
 		      <th>City</th>
 		      <th>Temperature</th>
 		      <th>Humidity</th>
 		    </tr>
 		  </thead>
 		  <tbody>
-		    <tr>
-		      <td>Mark</td>
-		      <td>Otto</td>
-		      <td>@mdo</td>
-		    </tr>
+		   	{new_table}
 		  </tbody>
 		</table>
       </div>
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,7 +3,7 @@ import { GET_WEATHER } from '../actions/index';
 export default function(state = [], action){
 	switch(action.type){
 		case GET_WEATHER:
-		 return [ actions.payload.data , ...state];
+		 return [ action.payload.data , ...state];
 	}
 	return state;
 }
